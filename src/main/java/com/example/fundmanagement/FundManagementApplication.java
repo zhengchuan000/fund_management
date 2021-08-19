a26b19d0e4a2bb9c620fff5a515ab75dc1f8e8bd
zhengchuan000 <zhengchuan000@gmail.com>
2021-08-19 16:21:55 +0800

    jenkins

--- src/main/java/com/example/fundmanagement/FundManagementApplication.java
+++ src/main/java/com/example/fundmanagement/FundManagementApplication.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class FundManagementApplication {
 
     public static void main(String[] args) {
-
+        System.out.println("!!!!!!!!!!!!!!!!Hello World!!!!!!!!!!!!!!");
         SpringApplication.run(FundManagementApplication.class, args);
     }
 
